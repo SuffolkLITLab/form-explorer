4823137148e5fc9c64308af35f89a094c7a918f7
David Colarusso <colarusso@users.noreply.github.com>
2022-08-23 15:07:38 -0400

    updated MA forms

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -287,7 +287,7 @@ function norm(a) {
 
       // Usage
 
-      $.cachedScript( "https://suffolklitlab.org/form-explorer/js/formsinfo.js?v=2022-08-23" ).done(function( script, textStatus ) {
+      $.cachedScript( "https://suffolklitlab.org/form-explorer/js/formsinfo.js?v=2022-08-23b" ).done(function( script, textStatus ) {
       //$.cachedScript( "https://findmycite.org/js/word2vec.js?=2022-08-22" ).done(function( script, textStatus ) {
         console.log( textStatus );
 
